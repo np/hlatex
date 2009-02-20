75a104e7456a354a2aebe57e8aac41c1d88a12c7
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2009-02-20 14:14:32 +0100

    MonoidUtils: add mconcatMap.

--- Language/LaTeX/Builder/MonoidUtils.hs
+++ Language/LaTeX/Builder/MonoidUtils.hs
@@ -1,4 +1,4 @@
-module Language.LaTeX.Builder.MonoidUtils ((<>)) where
+module Language.LaTeX.Builder.MonoidUtils ((<>), mconcatMap) where
 
 import Data.Monoid
 
@@ -6,3 +6,5 @@ infixr 5 <>
 (<>) :: Monoid m => m -> m -> m
 (<>) = mappend
 
+mconcatMap :: Monoid m => (a -> m) -> [a] -> m
+mconcatMap f = mconcat . map f
