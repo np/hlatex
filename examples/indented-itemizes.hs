91c17d3bbdc52c0ae279b30102078cc87785faa2
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2009-03-29 14:04:02 +0100

    add a small example

---
+++ examples/indented-itemizes.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -Wall -fno-warn-missing-signatures #-}
+import Language.LaTeX
+import qualified Language.LaTeX.Builder as B
+import Data.String ()
+
+main = quickView testViewOpts "indented-itemizes" root
+
+root = B.root preamb $ B.document doc
+
+preamb = B.documentclass (Just (B.pt 11)) (Just B.a4paper) B.article
+
+doc = mk "bla bla:" ["Aaaa", "Bbbb", "Cccc"]
+   <> mk "bli bli:" ["Dddd", "Eeee", "Ffff"]
+
+mk :: LatexItem -> [LatexItem] -> ParItem
+-- mk heading contents = B.para heading <> B.itemize (map (B.item . B.para) contents)
+mk heading (item0 : items) = B.tabular [B.l, B.l] (B.cells [heading, bullet <> item0]
+                                                  :map mkrow items)
+  where mkrow item = B.cells [mempty, bullet <> item] 
+        bullet = "- "
