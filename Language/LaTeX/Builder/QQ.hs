19afac762f778c05ccf9c96381899e5185946482
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2009-05-15 14:56:05 +0100

    L.L.B.QQ: fix the istr QQ by not using unlines.

--- Language/LaTeX/Builder/QQ.hs
+++ Language/LaTeX/Builder/QQ.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Language.LaTeX.Builder.QQ (frTop, frAntiq, frQQ, tex, str, istr) where
 
+import Data.List
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote
 import Language.LaTeX.Builder (rawTex)
@@ -17,7 +18,10 @@ frQQ = QuasiQuoter TH.stringE (TH.litP . TH.stringL)
 str = frQQ
 
 istr = QuasiQuoter (TH.stringE . stripIdent) (error "istr: not available in patterns")
-  where stripIdent = unlines . map (drop 1 . dropWhile (/='|')) . lines
+  where stripIdent = unlines' . skipFirst (map (drop 1 . dropWhile (/='|'))) . lines
+        unlines'   = intercalate "\n"
+        skipFirst _ []     = []
+        skipFirst f (x:xs) = x : f xs
 
 tex = QuasiQuoter (TH.appE (TH.varE 'rawTex) . TH.stringE) (error "tex: not available in patterns")
 
