a1a10b8e4d1095295c96c6cba7b91ea68c3671ea
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2009-05-15 15:04:43 +0100

    Adding examples/template.hs

---
+++ examples/template.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -Wall -fno-warn-missing-signatures #-}
+import Language.LaTeX
+import qualified Language.LaTeX.Builder as B
+import qualified Language.LaTeX.Builder.Math as M
+import qualified Language.LaTeX.Builder.Graphics as G
+-- import qualified Language.LaTeX.Builder.Rotating as R
+-- import qualified Language.LaTeX.Builder.Color as C
+
+import Data.Ratio ((%))
+import Data.Char
+import Data.List.Split
+import Data.List
+
+docName = "FILL ME"
+
+main = quickView testViewOpts docName root
+
+root = B.root preamb body
+
+preamb = B.documentclass (Just (B.pt 11)) (Just B.a4paper) B.book
+
+body = B.document <! do
+  tell B.tableofcontents
