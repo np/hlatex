56a5d218d03654eb11f1945cdb5c4e92ddf4b390
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2009-05-15 07:18:27 +0100

    Add the istr QQ

--- Language/LaTeX/Builder/QQ.hs
+++ Language/LaTeX/Builder/QQ.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Language.LaTeX.Builder.QQ (frTop, frAntiq, frQQ, tex, str) where
+module Language.LaTeX.Builder.QQ (frTop, frAntiq, frQQ, tex, str, istr) where
 
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote
@@ -11,10 +11,13 @@ frTop = id
 frAntiq :: a -> a
 frAntiq = id
 
-frQQ,str :: QuasiQuoter
+frQQ,str,istr,tex :: QuasiQuoter
+
 frQQ = QuasiQuoter TH.stringE (TH.litP . TH.stringL)
 str = frQQ
 
-tex :: QuasiQuoter
+istr = QuasiQuoter (TH.stringE . stripIdent) (error "istr: not available in patterns")
+  where stripIdent = unlines . map (drop 1 . dropWhile (/='|')) . lines
+
 tex = QuasiQuoter (TH.appE (TH.varE 'rawTex) . TH.stringE) (error "tex: not available in patterns")
 
