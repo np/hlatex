62fe170c5f6a8aa8393fad2faca51c4519c593a7
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2009-05-17 21:56:19 +0100

    L.L.B.QQ.istr: strictier parsing.

--- Language/LaTeX/Builder/QQ.hs
+++ Language/LaTeX/Builder/QQ.hs
@@ -2,6 +2,7 @@
 module Language.LaTeX.Builder.QQ (frTop, frAntiq, frQQ, tex, str, istr) where
 
 import Data.List
+import Data.Char
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote
 import Language.LaTeX.Builder (rawTex)
@@ -18,10 +19,13 @@ frQQ = QuasiQuoter TH.stringE (TH.litP . TH.stringL)
 str = frQQ
 
 istr = QuasiQuoter (TH.stringE . stripIdent) (error "istr: not available in patterns")
-  where stripIdent = unlines' . skipFirst (map (drop 1 . dropWhile (/='|'))) . lines
+  where stripIdent = unlines' . skipFirst (map (dropBar . dropWhile isSpace)) . lines
         unlines'   = intercalate "\n"
         skipFirst _ []     = []
         skipFirst f (x:xs) = x : f xs
+        dropBar []       = []
+        dropBar ('|':xs) = xs
+        dropBar _        = error "istr: syntax error '|' expected after spaces"
 
 tex = QuasiQuoter (TH.appE (TH.varE 'rawTex) . TH.stringE) (error "tex: not available in patterns")
 
