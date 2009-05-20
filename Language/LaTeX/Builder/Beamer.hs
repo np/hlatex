7c5f02368176fdb285020059a55b75c5c0c4ba8d
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2009-05-20 16:42:41 +0100

    L.L.B.Beamer: major rework and additions.

--- Language/LaTeX/Builder/Beamer.hs
+++ Language/LaTeX/Builder/Beamer.hs
@@ -7,8 +7,9 @@ import Language.LaTeX.Builder.MonoidUtils
 import Language.LaTeX.Types
 import qualified Language.LaTeX.Builder as B
 import Language.LaTeX.Builder.QQ
-import Data.List (sort, intercalate)
+import Data.List (intercalate)
 import Data.Maybe
+import Data.Monoid
 
 {-
 pkg :: PackageName
@@ -21,37 +22,54 @@ documentclass =  OtherDocumentClass "beamer"
 frametitle :: LatexItem -> ParItem
 frametitle = B.parCmdArg "frametitle"
 
-data FrameOpt = Overlays Overlays
+type TargetName = String
+type Label = String
+
+data FrameOpt = Label Label
               | Fragile
   deriving (Eq,Ord) -- Overlays is first
 
 data Overlays = RawOverlays String
+              | NoOverlays
   deriving (Eq,Ord)
 
 type BeamerOpt = (String, String)
 
-ppFrameOpt :: FrameOpt -> [Arg LatexItem]
-ppFrameOpt (Overlays overlays) = [ppOverlaysArg overlays]
-ppFrameOpt Fragile             = [B.optional (B.rawTex "fragile")]
+ppFrameOpt :: FrameOpt -> BeamerOpt
+ppFrameOpt (Label lbl) = ("label",lbl)
+ppFrameOpt Fragile     = ("fragile","")
 
-ppOverlays :: Overlays -> LatexItem
-ppOverlays (RawOverlays s) = B.rawTex . ('<':) . (++">") $ s
+ppFrameOpts :: [FrameOpt] -> [Arg LatexItem]
+ppFrameOpts = beamerOpts . map ppFrameOpt
+
+ppOverlaysOpt :: Overlays -> Maybe LatexItem
+ppOverlaysOpt (RawOverlays s) = Just . B.rawTex . ('<':) . (++">") $ s
+ppOverlaysOpt NoOverlays      = Nothing
 
 ppOverlaysArg :: Overlays -> Arg LatexItem
 ppOverlaysArg (RawOverlays s) = B.rawArg . ('<':) . (++">") $ s
+ppOverlaysArg NoOverlays      = B.noArg
 
 rawOverlays :: String -> Overlays
 rawOverlays =  RawOverlays
 
+noOverlays :: Overlays
+noOverlays = NoOverlays
+
+label :: Label -> FrameOpt
+label = Label
+
 -- more options to add ?
-frame' :: [FrameOpt] -> ParItem  -> ParItem 
-frame' opts = B.parEnvironmentPar "frame" (ppFrameOpt =<< sort opts)
+frame :: Overlays -> Overlays -> [FrameOpt] -> LatexItem -> LatexItem -> ParItem  -> ParItem 
+frame ov mov fopts title subtitle =
+  B.parEnvironmentPar "frame" $ [ ppOverlaysArg ov
+                                , maybe B.noArg B.optional $ ppOverlaysOpt mov
+                                ] ++ ppFrameOpts fopts ++
+                                [ B.mandatory title 
+                                , B.mandatory subtitle ]
 
 frameO :: Overlays -> ParItem  -> ParItem 
-frameO overlays = B.parEnvironmentPar "frame" [B.optional (ppOverlays overlays)]
-
-frame :: ParItem -> ParItem 
-frame = B.parEnvironmentPar "frame" []
+frameO overlays = B.parEnvironmentPar "frame" [maybe B.noArg B.optional $ ppOverlaysOpt overlays]
 
 example :: ParItem -> ParItem
 example = B.parEnvironmentPar "example" []
@@ -60,7 +78,7 @@ block :: LatexItem -> ParItem -> ParItem
 block title = B.parEnvironmentPar "block" [B.mandatory title]
 
 slide :: LatexItem -> ParItem -> ParItem
-slide tit body = frame (frametitle tit <> body)
+slide tit body = frame noOverlays noOverlays [] tit mempty body
 
 slideO :: LatexItem -> Overlays -> ParItem -> ParItem
 slideO tit ovs body = frameO ovs (frametitle tit <> body)
@@ -76,13 +94,13 @@ alert = B.latexCmdArg "alert"
 
 -- A shortcut for @itemize' . ppOverlays@
 itemize :: Overlays -> [ListItem] -> ParItem
-itemize = B.itemize' . ppOverlays
+itemize = B.itemize' . ppOverlaysOpt
 -- A shortcut for @enumerate' . ppOverlays@
 enumerate :: Overlays -> [ListItem] -> ParItem
-enumerate = B.enumerate' . ppOverlays
+enumerate = B.enumerate' . ppOverlaysOpt
 -- A shortcut for @description' . ppOverlays@
 description :: Overlays -> [ListItem] -> ParItem
-description = B.description' . ppOverlays
+description = B.description' . ppOverlaysOpt
 
 -- AtBeginSubsection, AtBeginSection
 
@@ -111,6 +129,67 @@ usefonttheme  = beamerPreambleCmdArgs "usefonttheme"
 useinnertheme = beamerPreambleCmdArgs "useinnertheme"
 useoutertheme = beamerPreambleCmdArgs "useoutertheme"
 usecolortheme = beamerPreambleCmdArgs "usecolortheme"
+
+{- | Draws a button with the given button text .
+
+  Example: @hyperlink "somewhere" (beamerbutton "Go somewhere")@
+
+  p97 beamer userguide
+-}
+beamerbutton :: LatexItem -> LatexItem
+beamerbutton = B.latexCmdArg "beamerbutton"
+
+{- | Draws a button with the given button text. Before the text, a small symbol (usually a
+     right-pointing arrow) is inserted that indicates that pressing this button will jump
+     to another *area* of the presentation.
+
+  Example: @hyperlink "detour" (beamergotobutton "Go to detour")@
+
+  p98 beamer userguide
+-}
+beamergotobutton :: LatexItem -> LatexItem
+beamergotobutton = B.latexCmdArg "beamergotobutton"
+
+{- | The symbol drawn for this button is usually a double right arrow. Use this button if
+     pressing it will skip over a well-defined part of your talk.
+
+  p98 beamer userguide
+-}
+beamerskipbutton :: LatexItem -> LatexItem
+beamerskipbutton = B.latexCmdArg "beamerskipbutton"
+
+{- | The symbol drawn for this button is usually a left-pointing arrow. Use this button
+     if pressing it will return from a detour.
+
+  p98 beamer userguide
+-}
+beamerreturnbutton :: LatexItem -> LatexItem
+beamerreturnbutton = B.latexCmdArg "beamerreturnbutton"
+
+{- |
+  Only one overlay specification may be given. The link text is typeset in the
+  usual way. If you click anywhere on this text, you will jump to the slide on
+  which the \hypertarget command was used with the parameter target name . If an
+  overlay specification is present, the hyperlink (including the link text) is
+  completely suppressed on the non-specified slides.
+
+  p99 beamer userguide
+-}
+hyperlink :: Overlays -> TargetName -> LatexItem -> Overlays -> LatexItem
+hyperlink ov1 target linkText ov2 =
+  B.latexCmdArgs "hyperlink" [ ppOverlaysArg ov1
+                             , B.mandatory (B.rawTex target)
+                             , B.mandatory linkText
+                             , ppOverlaysArg ov2
+                             ] 
+
+againframe :: Overlays -> Overlays -> [FrameOpt] -> Label -> ParItem
+againframe ov1 ov2 fopts lbl =
+  B.parCmdArgs "againframe" . concat $ [ ppOverlaysArg ov1
+                                       , ppOverlaysArg ov2
+                                       ] 
+                                       : ppFrameOpts fopts : [[B.mandatory (B.rawTex lbl)]]
+                                
 
 -- | Disable those litte icons at the bottom right of your presentation.
 beamertemplatenavigationsymbolsempty :: PreambleItem
