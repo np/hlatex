fe9df74b10c188c66c00112318f70272f3d68f7a
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2009-10-10 12:29:40 +0100

    L.L.B.MonoidUtils: Add <||> and <&&>, remove mconcatMap which is just D.Traversable.foldMap

--- Language/LaTeX/Builder/MonoidUtils.hs
+++ Language/LaTeX/Builder/MonoidUtils.hs
@@ -1,4 +1,4 @@
-module Language.LaTeX.Builder.MonoidUtils ((<>), mconcatMap, mapNonEmpty) where
+module Language.LaTeX.Builder.MonoidUtils ((<>), (<||>), (<&&>), mapNonEmpty) where
 
 import Data.Monoid
 
@@ -6,8 +6,13 @@ infixr 5 <>
 (<>) :: Monoid m => m -> m -> m
 (<>) = mappend
 
-mconcatMap :: Monoid m => (a -> m) -> [a] -> m
-mconcatMap f = mconcat . map f
+(<||>) :: (Monoid a, Eq a) => a -> a -> a
+a <||> b | a == mempty = b
+         | otherwise   = a
+
+(<&&>) :: (Monoid a, Eq a, Monoid b) => a -> b -> b
+a <&&> b | a == mempty = mempty
+         | otherwise   = b
 
 mapNonEmpty :: (Eq a, Monoid a, Monoid b) => (a -> b) -> a -> b
 mapNonEmpty f x | x == mempty = mempty
