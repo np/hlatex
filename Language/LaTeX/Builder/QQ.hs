d88e365f5f08884f69def68efa10cacb78c7a991
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2010-04-29 18:55:47 +0100

    Add qm

--- Language/LaTeX/Builder/QQ.hs
+++ Language/LaTeX/Builder/QQ.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Language.LaTeX.Builder.QQ (frTop, frAntiq, frQQ, tex, str, istr) where
+module Language.LaTeX.Builder.QQ (frTop, frAntiq, frQQ, tex, str, istr, qm) where
 
 import Data.List
 import Data.Char
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote
 import Language.LaTeX.Builder.Internal (rawTex)
+import Language.LaTeX.Builder.Math (mstring)
 
 frTop :: a -> a
 frTop = id
@@ -13,7 +14,7 @@ frTop = id
 frAntiq :: a -> a
 frAntiq = id
 
-frQQ,str,istr,tex :: QuasiQuoter
+frQQ,str,istr,tex,qm :: QuasiQuoter
 
 frQQ = QuasiQuoter TH.stringE (TH.litP . TH.stringL)
 str = frQQ
@@ -29,3 +30,4 @@ istr = QuasiQuoter (TH.stringE . stripIdent) (error "istr: not available in patt
 
 tex = QuasiQuoter (TH.appE (TH.varE 'rawTex) . TH.stringE) (error "tex: not available in patterns")
 
+qm = QuasiQuoter (TH.appE (TH.varE 'mstring) . TH.stringE) (error "qm: not available in patterns")
