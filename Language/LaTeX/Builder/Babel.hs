c72ba8f0b448fb74db54ce851fb3cb3148373203
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2010-06-05 15:01:19 +0100

    Add Language/LaTeX/Builder/Babel.hs

---
+++ Language/LaTeX/Builder/Babel.hs
@@ -0,0 +1,39 @@
+-- TODO add more langs
+module Language.LaTeX.Builder.Babel
+   (Lang, BabelOpt
+   ,useBabel
+   ,langName
+   -- langs
+   ,francais
+   ,french
+   -- last resort
+   ,customLang
+   ,customBabelOpt
+   ,pkg
+   ) where
+
+import Data.String
+import Language.LaTeX.Types
+import qualified Language.LaTeX.Builder.Internal as BI
+
+data BabelOpt = BabelOpt { babelOpt :: Arg LatexItem }
+
+data Lang = Lang { langName :: String }
+  deriving (Show, Eq)
+
+francais, french :: Lang
+francais = Lang "francais"
+french   = Lang "french"
+
+customLang :: String -> Lang
+customLang = Lang
+
+customBabelOpt :: Arg LatexItem -> BabelOpt
+customBabelOpt = BabelOpt
+
+pkg :: PackageName
+pkg = BI.pkgName "babel"
+
+useBabel :: Lang -> [BabelOpt] -> PreambleItem
+useBabel lang opts = BI.usepackage (BI.optional (fromString (langName lang))
+                                   : map babelOpt opts) pkg
