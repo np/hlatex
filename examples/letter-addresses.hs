78507dcc6eaf79f6bd86d23d7fa8a8a2e31bdbb1
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2010-06-05 15:31:55 +0100

    ./examples/letter-addresses.hs

--- examples/letter-addresses.hs
+++ examples/letter-addresses.hs
@@ -17,6 +17,7 @@ import Data.List
 import Data.String
 import Data.Maybe
 import Data.Foldable (foldMap)
+import Data.Monoid.Unicode
 import Control.Applicative
 import Control.Monad
 import Text.CSV (parseCSV, CSV, Record, Field)
@@ -27,15 +28,12 @@ import qualified System.IO.UTF8 as UTF8
 main = run =<< getArgs
 
 run args = do addrs <- loadAddrs args
-              quickView testViewOpts "letter-addresses" $ root addrs
+              quickView testViewOpts "letter-addresses" $ body addrs
 
-paraNoindent = B.para . (B.noindent <>)
+-- paraNoindent = B.para . (B.noindent ⊕)
 
-root addrs = B.root preamb $ body addrs
-
-preamb = B.documentclass (Just (L.pt 12)) (Just B.a4paper) B.article
-      <> B.useBabel B.francais []
-      <> BI.usepackage [BI.optional "utf8"] (BI.pkgName "inputenc")
+preamb = B.useBabel B.francais []
+       ⊕ BI.usepackage ["utf8"] (BI.pkgName "inputenc")
 
 data Addr = Addr { name    :: String
                  , street  :: String
@@ -44,13 +42,16 @@ data Addr = Addr { name    :: String
                  , country :: Maybe String
                  }
 
+texLines = mconcat . intersperse B.newline
+
 texAddr :: Addr -> ParItem
-texAddr (Addr{..}) =
-           (B.para $ fromString name
-     <> B.newline <> fromString street
-     <> B.newline <> fromString zipcode <> B.space <> fromString city
-     <> B.newline <> foldMap fromString country)
-     <> B.vspace (L.em 0.6)
+texAddr Addr{..} =
+  B.para (texLines
+   [fromString name
+   ,fromString street
+   ,fromString zipcode ⊕ B.space ⊕ fromString city
+   ,foldMap fromString country])
+  ⊕ B.vspace (L.em 0.6)
 
 texAddrs :: [Addr] -> ParItem
 texAddrs = foldMap tex8Addrs . chunk 20 -- . prolongateByMod def 2
@@ -59,12 +60,13 @@ texAddrs = foldMap tex8Addrs . chunk 20 -- . prolongateByMod def 2
     wi = L.linewidth / 2
     tex8Addrs addrs =
       B.tabular [B.l, B.l] (map (B.cells . tex2Addrs) (chunk 2 addrs))
-      <>
+      ⊕
       B.newpage
 
     tex2Addrs [a] = [B.minipage wi . texAddr $ a, mempty]
     tex2Addrs a   = map (B.minipage wi . texAddr) a
 
+{-
 -- think more about this
 foldl'Append :: (a -> b -> a) -> a -> (a -> [b]) -> [b] -> [b]
 foldl'Append _ z k []     = k z
@@ -73,6 +75,7 @@ foldl'Append f z k (x:xs) = z `seq` (x : foldl'Append f (f z x) k xs)
 prolongateByMod :: a -> Int -> [a] -> [a]
 prolongateByMod x modu = foldl'Append (const . succ) 0 f
   where f len = replicate (len `mod` modu) x
+  -}
 
 type HeaderRecord = Record
 csvSelector :: HeaderRecord -> Field -> Maybe (Record -> Field)
@@ -92,10 +95,11 @@ addrLoaderFromCSV header =
         zipcode   <- sel "Code Postal"
         city      <- sel "Ville"
         let build r = Addr { name    = name r ++ " " ++ firstname r
-                           , street  = (if null pre then "" else pre ++ " ") ++ street r
+                           , street  = (if null prer then "" else prer ++ " ") ++ street r
                            , zipcode = zipcode r
                            , city    = city r
                            , country = Nothing }
+              where prer = pre r
         return build
   where sel = csvSelector header
 
@@ -104,4 +108,5 @@ loadAddrs = fmap (fromMaybe [] . fmap concat . sequence) .  mapM f
   where f fp = (addrFromCSV =<<) . e2m . parseCSV fp <$> UTF8.readFile fp
         e2m = either (const Nothing) Just
 
-body addrs = B.document (B.pagestyle "empty" <> texAddrs addrs)
+body addrs = B.document dc preamb (B.pagestyle "empty" ⊕ texAddrs addrs)
+  where dc = B.article (Just (L.pt 12)) (Just B.a4paper) []
