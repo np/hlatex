5f32c5da6742c25a62bb4f4c7fbc0cf1b2e4ae64
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2011-03-08 23:31:28 +0100

    Add [qp|...|] for preambles

--- Language/LaTeX/Builder/QQ.hs
+++ Language/LaTeX/Builder/QQ.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Language.LaTeX.Builder.QQ (frTop, frAntiq, frQQ, tex, str, istr, qm) where
+module Language.LaTeX.Builder.QQ (frTop, frAntiq, frQQ, tex, qp, str, istr, qm) where
 
 import Data.List
 import Data.Char
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote
-import Language.LaTeX.Builder.Internal (rawTex)
+import Language.LaTeX.Builder.Internal (rawTex, rawPreamble)
 import Language.LaTeX.Builder.Math (mstring)
 
 frTop :: a -> a
@@ -14,7 +14,7 @@ frTop = id
 frAntiq :: a -> a
 frAntiq = id
 
-frQQ,str,istr,tex,qm :: QuasiQuoter
+frQQ,str,istr,tex,qm,qp :: QuasiQuoter
 
 frQQ = QuasiQuoter TH.stringE (TH.litP . TH.stringL)
 str = frQQ
@@ -31,3 +31,5 @@ istr = QuasiQuoter (TH.stringE . stripIdent) (error "istr: not available in patt
 tex = QuasiQuoter (TH.appE (TH.varE 'rawTex) . TH.stringE) (error "tex: not available in patterns")
 
 qm = QuasiQuoter (TH.appE (TH.varE 'mstring) . TH.stringE) (error "qm: not available in patterns")
+
+qp = QuasiQuoter (TH.appE (TH.varE 'rawPreamble) . TH.stringE) (error "qm: not available in patterns")
